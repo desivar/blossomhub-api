755e89223520e65f6a01f006e513c0a267270f06
desivar <jillian_desire@yahoo.es>
2025-06-06 10:04:29 -0600

    FIXED TYPO

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log('MongoDB Atlas connected successfully!');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
