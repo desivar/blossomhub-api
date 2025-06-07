828ea1bb9f2f163e305d57c662217f6b99339379
desivar <jillian_desire@yahoo.es>
2025-06-07 16:32:57 -0600

    delete jwt file and update swagger

--- utils/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (user) => {
-    return jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, {
-        expiresIn: '1h', // Token expires in 1 hour
-    });
-};
-
-module.exports = { generateToken };
